17a99f62d4f19e55f3e11ad61274931805e5959d
Rafael Briet <rafael-briet@hotmail.com>
2019-07-26 17:13:28 -0300

    Exercicio sobre lista

---
+++ Secao06/ExercicioLista/Funcionario.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace ExercicioLista
+{
+	class Funcionario
+	{
+		public int Id { get; set; }
+		public string Nome { get; set; }
+		public double Salario { get; set; }
+
+		public Funcionario(int id, string nome, double salario)
+		{
+			Id = id;
+			Nome = nome;
+			Salario = salario;
+		}
+
+		public void AumentoSalario(double porcentagem)
+		{
+			Salario += (Salario * porcentagem) / 100.0;
+		}
+
+		public override string ToString()
+		{
+			return Id + ", " + Nome + ", " + Salario.ToString("F2", CultureInfo.InvariantCulture);
+		}
+	}
+}
---
+++ Secao06/ExercicioLista/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ExercicioLista
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<Funcionario> funcionarios = new List<Funcionario>();
+
+			int id;
+			string nome;
+			double salario;
+			double porcentagem;
+
+			Console.Write("Quantos funcionários serão adicionados? ");
+			int n = int.Parse(Console.ReadLine());
+
+			for (int i = 0; i < n; i++)
+			{
+				Console.WriteLine("Funcionário #{0}:", i + 1);
+
+				Console.Write("ID: ");
+				id = int.Parse(Console.ReadLine());
+
+				Console.Write("Nome: ");
+				nome = Console.ReadLine();
+
+				Console.Write("Salario: ");
+				salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+				funcionarios.Add(new Funcionario(id, nome, salario));
+
+				Console.WriteLine();
+			}
+
+			Console.Write("Entre o ID do funcionário que terá aumento salarial: ");
+			id = int.Parse(Console.ReadLine());
+
+			Funcionario pesquisa = funcionarios.Find(x => x.Id == id);
+
+			if (pesquisa != null)
+			{
+				Console.Write("Entre a porcentagem do aumento: ");
+				porcentagem = int.Parse(Console.ReadLine());
+
+				pesquisa.AumentoSalario(porcentagem);
+			}
+			else
+			{
+				Console.WriteLine("Esse ID não existe!");
+			}
+
+			Console.WriteLine();
+
+			Console.WriteLine("Lista de funcionários atualizada:");
+			foreach (Funcionario f in funcionarios)
+			{
+				Console.WriteLine(f);
+			}
+		}
+	}
+}
