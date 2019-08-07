f0bfed28157d1f5c4069af11ea8d90832fc84d45
Rafael Briet <rafael-briet@hotmail.com>
2019-08-06 23:59:00 -0300

    Criando a classe ChessPosition

---
+++ Secao12/ChessGame/Chess/ChessPosition.cs
@@ -0,0 +1,26 @@
+﻿using ChessGame.Board;
+
+namespace ChessGame.Chess
+{
+	class ChessPosition
+	{
+		public int Line { get; set; }
+		public char Collumn { get; set; }
+
+		public ChessPosition(int line, char collumn)
+		{
+			Line = line;
+			Collumn = collumn;
+		}
+
+		public Position ToPosition()
+		{
+			return new Position(8 - Line, Collumn - 'a');
+		}
+
+		public override string ToString()
+		{
+			return $"{Collumn}{Line}";
+		}
+	}
+}
