4c7e773d8b6587cd6b35a6923d91d33eaba63e6d
Rafael Briet <rafael-briet@hotmail.com>
2019-08-07 00:17:39 -0300

    Melhorando a impressção do tabuleiro

--- Secao12/ChessGame/Program.cs
+++ Secao12/ChessGame/Program.cs
@@ -10,24 +10,24 @@ namespace ChessGame
 		{
 			GameBoard gameBoard = new GameBoard(8, 8);
 
-			//try
-			//{
-			//	gameBoard.AddPiece(new Rook(gameBoard, Color.Black), new Position(0, 0));
-			//	gameBoard.AddPiece(new Rook(gameBoard, Color.Black), new Position(0, 7));
-			//	gameBoard.AddPiece(new King(gameBoard, Color.Black), new Position(0, 5));
+			try
+			{
+				gameBoard.AddPiece(new Rook(gameBoard, Color.Black), new Position(0, 0));
+				gameBoard.AddPiece(new Rook(gameBoard, Color.Black), new Position(0, 7));
+				gameBoard.AddPiece(new King(gameBoard, Color.Black), new Position(0, 5));
 
-			//	UI.InitBoard(gameBoard);
-			//}
-			//catch (Exception e)
-			//{
-			//	Console.WriteLine(e.Message);
-			//	Console.WriteLine(e.StackTrace);
-			//}
+				UI.PrintBoard(gameBoard);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+				Console.WriteLine(e.StackTrace);
+			}
 
-			ChessPosition pos = new ChessPosition(5, 'd');
+			//ChessPosition pos = new ChessPosition(5, 'd');
 
-			Console.WriteLine(pos);
-			Console.WriteLine(pos.ToPosition());
+			//Console.WriteLine(pos);
+			//Console.WriteLine(pos.ToPosition());
 
 			Console.ReadLine();
 		}
--- Secao12/ChessGame/UI.cs
+++ Secao12/ChessGame/UI.cs
@@ -5,10 +5,12 @@ namespace ChessGame
 {
 	class UI
 	{
-		public static void InitBoard(GameBoard board)
+		public static void PrintBoard(GameBoard board)
 		{
 			for (int x = 0; x < board.Lines; x++)
 			{
+				Console.Write($"{8 - x} ");
+
 				for (int y = 0; y < board.Collumns; y++)
 				{
 					Piece piece = board.GetPieceAt(x, y);
@@ -19,11 +21,32 @@ namespace ChessGame
 					}
 					else
 					{
-						Console.Write($"{piece} ");
+						PrintPiece(piece);
+						Console.Write(" ");
 					}
 				}
 
 				Console.WriteLine();
+			}
+
+			Console.WriteLine("  A B C D E F G H");
+		}
+
+		public static void PrintPiece(Piece piece)
+		{
+			switch (piece.Color)
+			{
+				case Color.Black:
+					ConsoleColor defaultColor = Console.ForegroundColor;
+					Console.ForegroundColor = ConsoleColor.Yellow;
+					Console.Write(piece);
+					Console.ForegroundColor = defaultColor;
+					break;
+				case Color.White:
+					Console.Write(piece);
+					break;
+				default:
+					break;
 			}
 		}
 	}
