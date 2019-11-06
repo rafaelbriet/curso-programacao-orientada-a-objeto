f57ead3c768da28df403760e8c2b97e73314442f
Rafael Briet <rafael-briet@hotmail.com>
2019-11-06 14:59:29 -0300

    Adicionado Departments view and controller

---
+++ Secao17/SalesWebApp/Data/SalesWebAppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesWebApp.Models
+{
+    public class SalesWebAppContext : DbContext
+    {
+        public SalesWebAppContext (DbContextOptions<SalesWebAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebApp.Models.Department> Department { get; set; }
+    }
+}
--- Secao17/SalesWebApp/Startup.cs
+++ Secao17/SalesWebApp/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SalesWebApp.Models;
 
 namespace SalesWebApp
 {
@@ -33,6 +35,9 @@ namespace SalesWebApp
 
 
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+		    services.AddDbContext<SalesWebAppContext>(options =>
+		            options.UseSqlServer(Configuration.GetConnectionString("SalesWebAppContext")));
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
