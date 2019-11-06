7dc6dfa2e2fa2578e1b22fb001cae0d0402a2e7d
Rafael Briet <rafael-briet@hotmail.com>
2019-11-06 14:57:43 -0300

    Adicionado Department

--- Secao17/SalesWebApp/Controllers/HomeController.cs
+++ Secao17/SalesWebApp/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using SalesWebApp.Models;
+using SalesWebApp.Models.ViewModels;
 
 namespace SalesWebApp.Controllers
 {
